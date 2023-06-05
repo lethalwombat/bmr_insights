c7decb6bef7415fc585793f2d7acf1e29ee8bc2e
lethalwombat <lethalwombat@outlook.com>
2023-06-05 13:39:25 +1000

    bug fix

--- azure_infra/post_deployment/upload_assets.sh
+++ azure_infra/post_deployment/upload_assets.sh
@@ -6,7 +6,9 @@ scp -i ~/.ssh/id_rsa ~/.ssh/id_rsa.pub azureuser@$1:/home/azureuser/.ssh/ &&\
 
 # docker installation script
 scp -i ~/.ssh/id_rsa docker_install.sh azureuser@$1:/home/azureuser &&\
-ssh -i ~/.ssh/id_rsa azureuser@$1 &&\
 
 # repo clone script
 scp -i ~/.ssh/id_rsa repo_clone.sh azureuser@$1:/home/azureuser
+
+# ssh into the vm
+ssh -i ~/.ssh/id_rsa azureuser@$1 &&\
